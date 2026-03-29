e37e88dc6e3d4d03252cc733578703247086b07f
Katoo o <owengjap@gmail.com>
2026-03-29 20:53:23 +0700

    additional backend 2 fix

--- src/main/java/sync2/chatApp/repository/MessageBundleRepository.java
+++ src/main/java/sync2/chatApp/repository/MessageBundleRepository.java
@@ -1,13 +1,11 @@
 package sync2.chatApp.repository;
 
-import java.util.UUID;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import sync2.chatApp.entity.MessageBundle;
 
 @Repository
-public interface MessageBundleRepository extends JpaRepository<MessageBundle, UUID> {
+public interface MessageBundleRepository extends JpaRepository<MessageBundle, Long> {
 
 }
