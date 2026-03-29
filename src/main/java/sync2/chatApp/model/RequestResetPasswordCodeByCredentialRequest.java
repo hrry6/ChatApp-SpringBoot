7ecf5099e84a6d634a5fa3ca0c88fc5f3e8cf2fc
Katoo o <owengjap@gmail.com>
2026-03-29 18:09:44 +0700

    tambahan backend

---
+++ src/main/java/sync2/chatApp/model/RequestResetPasswordCodeByCredentialRequest.java
@@ -0,0 +1,23 @@
+package sync2.chatApp.model;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+public class RequestResetPasswordCodeByCredentialRequest {
+	@NotBlank
+	@Email
+	@Size(max = 100)
+	private String email;
+
+	@NotBlank
+	private String password;
+}
